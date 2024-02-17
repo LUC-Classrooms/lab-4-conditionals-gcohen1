158626d4b179dfe8c632fee48932f40fd2894fd1
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-02-17 20:57:39 +0000

    added changes

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
+    fill(284, 0, 0);
     
   }
   else { 
     // otherwise do this:
+    fill(0, 280, 0);
+    g = width/2;
+    w = 100;
+
     
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(g, height/2, w, 110);
 }
